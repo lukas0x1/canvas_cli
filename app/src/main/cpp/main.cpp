e5801fb026e10d552b32cfd1926469db5a8bb8fd
Lukas <107052834+lukas0x1@users.noreply.github.com>
2023-03-05 22:46:40 +0100

    Initial commit

---
+++ app/src/main/cpp/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Lukas on 22/07/2022.
+//
+
+#include "main.h"
+#include "includes/cipher/Cipher.h"
+#include "includes/imgui/imgui.h"
+#include "includes/misc/Logger.h"
+
+
+
+
+void Menu() {
+        ImGui::Checkbox("Relationships", (bool *)(Cipher::get_libBase() + 0x16A66F8));
+}
+
+
+void Init(){
+
+};
